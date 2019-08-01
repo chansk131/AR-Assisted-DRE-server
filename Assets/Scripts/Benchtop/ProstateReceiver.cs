dc51da971d7bd8064ad110b9c4d40e29a03354f0
Theerapat Muangpoon <chansk131@gmail.com>
2019-08-01 12:19:21 +0100

    add prostate sync

---
+++ Assets/Scripts/Benchtop/ProstateReceiver.cs
@@ -0,0 +1,25 @@
+﻿using HoloToolkit.Sharing;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProstateReceiver : MonoBehaviour
+{
+    [SerializeField]
+    private Dropdown prostateDropdown;
+    [SerializeField]
+    private CustomMessages.TestMessageID testMessageId = CustomMessages.TestMessageID.Prostate;
+
+    private void Start()
+    {
+        CustomMessages.Instance.MessageHandlers[testMessageId] = ProcessProstate;
+    }
+
+    public void ProcessProstate(NetworkInMessage msg)
+    {
+        long userID = msg.ReadInt64();
+        var prostate = CustomMessages.Instance.ReadInt(msg);          // change prostate state         Game.Instance.Prostate = (ProstateType)prostate;
+
+        // change prostate menu
+        prostateDropdown.value = prostate;
+    }
+}
